b352b0c7925320323922d3ec3d8bd8bbdbf9a4c8
Kirill Smelkov <kirr@nexedi.com>
2022-08-05 12:00:36 +0300

    .

--- src/ZEO/tests/testZEO.py
+++ src/ZEO/tests/testZEO.py
@@ -28,8 +28,11 @@ from ZODB.tests import StorageTestBase, BasicStorage,  \
      MTStorage, ReadOnlyStorage, IteratorStorage, RecoveryStorage
 from ZODB.tests.MinPO import MinPO
 from ZODB.tests.StorageTestBase import zodb_unpickle
-from ZODB.utils import maxtid, p64, u64, z64
+from ZODB.utils import maxtid, p64, u64, z64, tid_repr
 from zope.testing import renormalizing
+from ZODB.tests.util import with_high_concurrency
+from ZODB import POSException
+from random import randint
 
 import doctest
 import logging
@@ -382,6 +385,235 @@ class FullGenericTests(
             underlying_func = wrapper_func.func_closure[0].cell_contents
 
         underlying_func(self)
+
+
+    @with_high_concurrency
+    def check_ZZ_race_loadopen_vs_local_invalidate(self):
+        db = ZODB.DB(self._storage)
+
+        # init initializes the database with two integer objects - obj1/obj2
+        # that are set to 0.
+        def init():
+            transaction.begin()
+            zconn = db.open()
+
+            root = zconn.root()
+            root['obj1'] = MinPO(0)
+            root['obj2'] = MinPO(0)
+
+            transaction.commit()
+            zconn.close()
+
+        # verify accesses obj1/obj2 and verifies that obj1.value == obj2.value
+        #
+        # access to obj1 is organized to always trigger loading from zstor.
+        # access to obj2 goes through zconn cache and so verifies whether the
+        # cache is not stale.
+        failed = threading.Event()
+        failure = [None]
+
+        def verify():
+            transaction.begin()
+            zconn = db.open()
+
+            root = zconn.root()
+            obj1 = root['obj1']
+            obj2 = root['obj2']
+
+            # obj1 - reload it from zstor
+            # obj2 - get it from zconn cache
+            obj1._p_invalidate()
+
+            # both objects must have the same values
+            v1 = obj1.value
+            v2 = obj2.value
+            if v1 != v2:
+                msg  = "verify: obj1.value (%d)  !=  obj2.value (%d)\n" % (
+                       v1, v2)
+                msg += "obj1._p_serial: %s  obj2._p_serial: %s  zconn.at: %s" % (
+                       tid_repr(obj1._p_serial), tid_repr(obj2._p_serial),
+                       tid_repr(zconn_at(zconn)))
+                failure[0] = msg
+                failed.set()
+
+            # we did not changed anything; also fails with commit:
+            transaction.abort()
+            zconn.close()
+
+        # modify changes obj1/obj2 by doing `objX.value += 1`.
+        #
+        # Since both objects start from 0, the invariant that
+        # `obj1.value == obj2.value` is always preserved.
+        def modify():
+            transaction.begin()
+            zconn = db.open()
+
+            root = zconn.root()
+            obj1 = root['obj1']
+            obj2 = root['obj2']
+            obj1.value += 1
+            obj2.value += 1
+            assert obj1.value == obj2.value
+
+            transaction.commit()
+            zconn.close()
+
+        # xrun runs f in a loop until either N iterations, or until failed is
+        # set.
+        def xrun(f, N):
+            try:
+                for i in range(N):
+                    # print('%s.%d' % (f.__name__, i))
+                    f()
+                    if failed.is_set():
+                        break
+            except:  # noqa: E722 do not use bare 'except'
+                failed.set()
+                raise
+
+        # loop verify and modify concurrently.
+        init()
+
+        N = 500
+        tverify = threading.Thread(
+            name='Tverify', target=xrun, args=(verify, N))
+        tmodify = threading.Thread(
+            name='Tmodify', target=xrun, args=(modify, N))
+        tverify.start()
+        tmodify.start()
+        tverify.join(60)
+        tmodify.join(60)
+
+        if failed.is_set():
+            self.fail(failure[0])
+
+    # verify storage for race in between load and external invalidations.
+    # https://github.com/zopefoundation/ZEO/issues/155
+    #
+    # This test is similar to check_race_loadopen_vs_local_invalidate but does
+    # not reuse its code because the probability to reproduce external
+    # invalidation bug with only 1 mutator + 1 verifier is low.
+    @with_high_concurrency
+    def check_ZZ_race_load_vs_external_invalidate(self):
+        # dbopen creates new client storage connection and wraps it with DB.
+        def dbopen():
+            try:
+                zstor = self._new_storage_client()
+            except NotImplementedError:
+                # the test will be skipped from main thread because dbopen is
+                # first used in init on the main thread before any other thread
+                # is spawned.
+                self.skipTest(
+                    "%s does not implement _new_storage_client" % type(self))
+            return ZODB.DB(zstor)
+
+        # init initializes the database with two integer objects - obj1/obj2
+        # that are set to 0.
+        def init():
+            db = dbopen()
+
+            transaction.begin()
+            zconn = db.open()
+
+            root = zconn.root()
+            root['obj1'] = MinPO(0)
+            root['obj2'] = MinPO(0)
+
+            transaction.commit()
+            zconn.close()
+
+            db.close()
+
+        # we'll run 8 T workers concurrently. As of 20210416, due to race
+        # conditions in ZEO, it triggers the bug where T sees stale obj2 with
+        # obj1.value != obj2.value
+        #
+        # The probability to reproduce the bug is significantly reduced with
+        # decreasing n(workers): almost never with nwork=2 and sometimes with
+        # nwork=4.
+        nwork = 8
+
+        # T is a worker that accesses obj1/obj2 in a loop and verifies
+        # `obj1.value == obj2.value` invariant.
+        #
+        # access to obj1 is organized to always trigger loading from zstor.
+        # access to obj2 goes through zconn cache and so verifies whether the
+        # cache is not stale.
+        #
+        # Once in a while T tries to modify obj{1,2}.value maintaining the
+        # invariant as test source of changes for other workers.
+        failed = threading.Event()
+        failure = [None] * nwork  # [tx] is failure from T(tx)
+
+        def T(tx, N):
+            db = dbopen()
+
+            def t_():
+                transaction.begin()
+                zconn = db.open()
+
+                root = zconn.root()
+                obj1 = root['obj1']
+                obj2 = root['obj2']
+
+                # obj1 - reload it from zstor
+                # obj2 - get it from zconn cache
+                obj1._p_invalidate()
+
+                # both objects must have the same values
+                i1 = obj1.value
+                i2 = obj2.value
+                if i1 != i2:
+                    # print('FAIL')
+                    msg  = "T%s: obj1.value (%d)  !=  obj2.value (%d)\n" % (
+                          tx, i1, i2)
+                    msg += "obj1._p_serial: %s  obj2._p_serial: %s  zconn.at: %s" % (
+                           tid_repr(obj1._p_serial), tid_repr(obj2._p_serial),
+                           tid_repr(zconn_at(zconn)))
+                    failure[tx] = msg
+                    failed.set()
+
+                # change objects once in a while
+                if randint(0, 4) == 0:
+                    # print("T%s: modify" % tx)
+                    obj1.value += 1
+                    obj2.value += 1
+
+                try:
+                    transaction.commit()
+                except POSException.ConflictError:
+                    # print('conflict -> ignore')
+                    transaction.abort()
+
+                zconn.close()
+
+            try:
+                for i in range(N):
+                    # print('T%s.%d' % (tx, i))
+                    t_()
+                    if failed.is_set():
+                        break
+            except:  # noqa: E722 do not use bare 'except'
+                failed.set()
+                raise
+            finally:
+                db.close()
+
+        # run the workers concurrently.
+        init()
+
+        N = 100
+        tg = []
+        for x in range(nwork):
+            t = threading.Thread(name='T%d' % x, target=T, args=(x, N))
+            t.start()
+            tg.append(t)
+
+        for t in tg:
+            t.join(60)
+
+        if failed.is_set():
+            self.fail([_ for _ in failure if _])
 
 
 class FileStorageRecoveryTests(StorageTestBase.StorageTestBase,
@@ -1904,6 +2136,28 @@ def test_suite():
         suite.addTest(dynamic_server_ports_suite)
 
     return suite
+
+
+# zconn_at returns tid as of which ZODB connection is viewing the database.
+#
+# In other words zconn_at returns database state corresponding to database view
+# of the connection.
+def zconn_at(zconn): # -> tid
+    assert isinstance(zconn, ZODB.Connection.Connection)
+    if zconn.opened is None: # zconn must be in "opened" state
+        raise POSException.ConnectionStateError("database connection is closed")
+
+    # XXX text
+    zstor = zconn._storage
+    if isinstance(zstor, ZODB.mvccadapter.MVCCAdapterInstance):
+        return before2at(zstor._start)
+    else:
+        raise AssertionError('TODO')
+
+# before2at converts tid that specifies database state as "before" into tid that
+# specifies database state as "at".
+def before2at(before): # -> at
+    return p64(u64(before) - 1)
 
 
 if __name__ == "__main__":
